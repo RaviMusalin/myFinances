fabf71366f36081f3579cde134fe221b5f549e9a
RaviMusalin <sheikhmusalin@gmail.com>
2025-01-22 23:25:22 -0500

    Auth Component Sign Up complete

---
+++ client/src/pages/auth/index.tsx
@@ -0,0 +1,18 @@
+import {SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from '@clerk/clerk-react'
+
+function Auth() {
+    return (
+        <div className='sign-in-container'> 
+            <SignedOut> 
+                <SignUpButton mode="modal"/>
+                <SignInButton mode="modal"/>
+            </SignedOut>
+
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+        </div>
+    )
+}
+
+export default Auth

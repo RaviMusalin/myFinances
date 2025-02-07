afd45f0f1d6baeae693c69d2ac84d6a662f4e424
RaviMusalin <sheikhmusalin@gmail.com>
2025-02-06 20:49:46 -0500

    Update index.ts and schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,21 @@
-import {express} from 
+import express, { Express } from "express"
+import mongoose from "mongoose"
+import dotenv from 'dotenv'
+
+dotenv.config({ path: './.env.local' })
+
+const app: Express = express()
+const port = process.env.PORT || 3001
+
+app.use(express.json())
+
+const mongoURI = process.env.MONGO_URI
+
+mongoose
+    .connect(`${mongoURI}`)
+    .then(() => console.log("CONNECTED TO MONGODB"))
+    .catch((err) => console.error("Failed to Connect", err))
+
+app.listen(port, () => {
+    console.log(`Server Running on Port ${port}`)
+})
